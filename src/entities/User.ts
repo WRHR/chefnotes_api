e7adbe7af7934e2f081da16b77ced130d71039c6
WRHR <wrhreeves@gmail.com>
2021-03-02 22:38:30 -0700

    Create user entity

---
+++ src/entities/User.ts
@@ -0,0 +1,24 @@
+import { Field, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class User extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field()
+  @Column({ unique: true })
+  username!: string;
+
+  @Column()
+  password!: string;
+
+}
