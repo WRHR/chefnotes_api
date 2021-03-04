ef541ff61f9ce2f3e711051de48340515377d753
WRHR <wrhreeves@gmail.com>
2021-03-03 21:31:34 -0700

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -23,6 +23,8 @@ const express_session_1 = __importDefault(require("express-session"));
 const connect_redis_1 = __importDefault(require("connect-redis"));
 const cors_1 = __importDefault(require("cors"));
 const constants_1 = require("./constants");
+const User_1 = require("./entities/User");
+const user_1 = require("./resolvers/user");
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     dotenv_1.default.config();
     yield typeorm_1.createConnection({
@@ -30,7 +32,7 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         database: process.env.DB,
         logging: true,
         synchronize: true,
-        entities: [],
+        entities: [User_1.User],
     });
     const app = express_1.default();
     const RedisStore = connect_redis_1.default(express_session_1.default);
@@ -57,7 +59,7 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
     }));
     const apolloServer = new apollo_server_express_1.ApolloServer({
         schema: yield type_graphql_1.buildSchema({
-            resolvers: [],
+            resolvers: [user_1.UserResolver],
             validate: false
         }),
         context: ({ req, res }) => ({ req, res, redis })
