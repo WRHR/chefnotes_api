319be9189164994a7c199b251da55b834762dd4a
WRHR <wrhreeves@gmail.com>
2021-03-07 22:04:26 -0700

    Create base and modified recipe entities

---
+++ src/entities/BaseRecipe.ts
@@ -0,0 +1,46 @@
+import { Field, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Ingredient } from "./Ingredient";
+import { Instruction } from "./Instruction";
+import { ModifiedRecipe } from "./ModifiedRecipe";
+import { User } from "./User";
+
+@ObjectType()
+@Entity()
+export class BaseRecipe extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field()
+  @Column()
+  name!: string;
+
+  @Field()
+  @Column()
+  description!: string;
+
+  @OneToMany(() => Ingredient, (ingredient) => ingredient.recipe)
+  ingredients: Ingredient[];
+
+  @OneToMany(() => Instruction, (instruction) => instruction.recipe)
+  instructions: Instruction[];
+
+  @OneToMany(()=> ModifiedRecipe, mr => mr.baseRecipe)
+  modifiedRecipes: ModifiedRecipe[]
+
+  @Field()
+  @PrimaryColumn()
+  creatorId: number;
+
+  @ManyToOne(() => User, (user) => user.recipes)
+  creator: User;
+}
---
+++ src/entities/ModifiedRecipe.ts
@@ -0,0 +1,46 @@
+import { Field, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { BaseRecipe } from "./BaseRecipe";
+import { Ingredient } from "./Ingredient";
+import { Instruction } from "./Instruction";
+import { User } from "./User";
+
+@ObjectType()
+@Entity()
+export class ModifiedRecipe extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field()
+  @Column()
+  name!: string;
+
+  @Field()
+  @Column()
+  description!: string;
+
+  @OneToMany(() => Ingredient, (ingredient) => ingredient.recipe)
+  ingredients: Ingredient[];
+
+  @OneToMany(() => Instruction, (instruction) => instruction.recipe)
+  instructions: Instruction[];
+
+  @ManyToOne(()=>BaseRecipe, br => br.modifiedRecipes)
+  baseRecipe: BaseRecipe
+
+  @Field()
+  @PrimaryColumn()
+  creatorId: number;
+
+  @ManyToOne(() => User, (user) => user.recipes)
+  creator: User;
+}
