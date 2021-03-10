a573fe64eb95a0e148f4e77d6bd1d38fbf931bc7
WRHR <wrhreeves@gmail.com>
2021-03-09 23:13:28 -0700

    Create instructionResolver

---
+++ src/resolvers/instruction.ts
@@ -0,0 +1,41 @@
+import { Instruction } from "../entities/Instruction";
+import { Arg, Field, InputType, Int, Mutation, Resolver } from "type-graphql";
+
+@InputType()
+class InstructionInput {
+  @Field()
+  description:string
+
+  @Field()
+  position:number
+}
+
+@Resolver(Instruction)
+export class InstructionResolver {
+  @Mutation(()=>Instruction)
+  async createInstruction(
+    @Arg("input") input: InstructionInput,
+    @Arg("recipeId", () => Int) recipeId: number
+  ): Promise<Instruction> {
+    return Instruction.create({
+      ...input,
+      recipeId
+    }).save()
+  }
+
+  @Mutation(()=>Instruction, {nullable:true})
+  async updateInstruction(
+    @Arg('id',()=>Int) id:number,
+    @Arg('input')input:InstructionInput,
+  ):Promise<Instruction|null>{
+    return (await Instruction.update({id},{...input})).raw[0]
+  }
+
+  @Mutation(()=>Boolean)
+  async deleteInstruction(
+    @Arg('id',()=>Int)id:number
+  ){
+    await Instruction.delete({id})
+    return true
+  }
+}
