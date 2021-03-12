24c0a80370de03c11877831cb54920819667fa6a
WRHR <wrhreeves@gmail.com>
2021-03-11 22:26:23 -0700

    Create mod recipe resolver

---
+++ src/resolvers/modifiedRecipe.ts
@@ -0,0 +1,114 @@
+import { MyContext } from "src/types";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Int,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
+import { getConnection } from "typeorm";
+import { ModifiedRecipe } from "../entities/ModifiedRecipe";
+
+@InputType()
+class RecipeInput {
+  @Field()
+  name!: string;
+
+  @Field()
+  description!: string;
+
+  @Field()
+  baseRecipeId:number
+}
+
+@ObjectType()
+class PaginatedRecipes {
+  @Field(() => [ModifiedRecipe])
+  recipes: ModifiedRecipe[];
+
+  @Field()
+  hasMore: boolean;
+}
+
+@Resolver(ModifiedRecipe)
+export class ModifiedRecipeResolver {
+  @Query(() => [ModifiedRecipe])
+  async recipesAll(): Promise<ModifiedRecipe[]> {
+    return ModifiedRecipe.find();
+  }
+
+  @Query(() => PaginatedRecipes)
+  async recipes(
+    @Arg("limit", () => Int) limit: number,
+    @Arg("cursor", () => String, { nullable: true }) cursor: string | null
+  ): Promise<PaginatedRecipes> {
+    const realLimit = Math.min(50, limit);
+    const realLimitPlusOne = realLimit + 1;
+    const replacements: any[] = [realLimitPlusOne];
+
+    if (cursor) {
+      replacements.push(new Date(parseInt(cursor)));
+    }
+
+    const recipes = await getConnection().query(
+      `
+      select 
+      `
+    );
+    return {
+      recipes: recipes.slice(0, realLimit),
+      hasMore: postMessage.length === realLimitPlusOne,
+    };
+  }
+
+  @Query(() => ModifiedRecipe, { nullable: true })
+  recipe(@Arg("id", () => Int) id: number): Promise<ModifiedRecipe | undefined> {
+    return ModifiedRecipe.findOne(id);
+  }
+
+  @Mutation(() => ModifiedRecipe)
+  async createRecipe(
+    @Arg("input") input: RecipeInput,
+    @Arg('baseRecipeId',()=>Int) baseRecipeId:number
+    @Ctx() { req }: MyContext
+  ): Promise<ModifiedRecipe> {
+    return ModifiedRecipe.create({
+      ...input,
+      baseRecipeId,
+      creatorId: req.session.userId,
+    }).save();
+  }
+
+  @Mutation(() => ModifiedRecipe)
+  async updateRecipe(
+    @Arg("id", () => Int) id: number,
+    @Arg("name") name: string,
+    @Arg("description") description: string,
+    @Ctx() { req }: MyContext
+  ): Promise<ModifiedRecipe | null> {
+    const result = await getConnection()
+      .createQueryBuilder()
+      .update(ModifiedRecipe)
+      .set({ name, description })
+      .where('id = :id and "creatorId" = :creatorId', {
+        id,
+        creatorId: req.session.userId,
+      })
+      .returning("*")
+      .execute();
+    return result.raw[0];
+  }
+
+  @Mutation(() => Boolean)
+  async deleteRecipe(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { req }: MyContext
+  ): Promise<Boolean> {
+    await ModifiedRecipe.delete({ id, creatorId: req.session.userId });
+    return true;
+  }
+}
